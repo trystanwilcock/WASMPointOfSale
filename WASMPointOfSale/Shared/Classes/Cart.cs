62d3300fef3bfcb3712fd25b471cf989edcda5cb
Trystan Wilcock <trystanwilcock@hotmail.co.uk>
2023-01-07 06:12:03 +0000

    Added initial new sale page.

---
+++ WASMPointOfSale/Shared/Classes/Cart.cs
@@ -0,0 +1,82 @@
+﻿using WASMPointOfSale.Shared.ViewModels;
+
+namespace WASMPointOfSale.Shared.Classes
+{
+    public class Cart
+    {
+        public List<CartLine> Lines;
+
+        public Cart()
+        {
+            Lines = new();
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return Lines.Sum(l => l.Quantity);
+            }
+        }
+
+        public decimal Total
+        {
+            get
+            {
+                return Lines.Sum(l => l.Total);
+            }
+        }
+
+        public void AddToCart(ProductViewModel product)
+        {
+            var existingLine = GetCartLine(product.Code);
+
+            if (existingLine != null)
+            {
+                existingLine.Quantity++;
+            }
+            else
+            {
+                Lines.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = 1
+                });
+            }
+        }
+
+        public void RemoveFromCart(ProductViewModel product)
+        {
+            var existingLine = GetCartLine(product.Code);
+
+            if (existingLine!.Quantity == 1)
+            {
+                Lines.Remove(existingLine);
+            }
+            else
+            {
+                existingLine.Quantity--;
+            }
+        }
+
+        private CartLine? GetCartLine(string code)
+        {
+            return Lines.FirstOrDefault(l => l.Product.Code == code);
+        }
+    }
+
+    public class CartLine
+    {
+        public ProductViewModel Product { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Total
+        {
+            get
+            {
+                return Product.Price * Quantity;
+            }
+        }
+    }
+}
--- WASMPointOfSale/Shared/ViewModels/ProductViewModel.cs
+++ WASMPointOfSale/Shared/ViewModels/ProductViewModel.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
 
+        public int ProductCategoryId { get; set; }
+
         public string Code { get; set; }
 
         public string Name { get; set; }
