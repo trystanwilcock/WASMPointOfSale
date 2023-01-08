6fd1c232cc09390d3c788c8486e63104563dd232
Trystan Wilcock <trystanwilcock@hotmail.co.uk>
2023-01-08 08:35:11 +0000

    Added Quantity field to Stock entity.

---
+++ WASMPointOfSale/Server/Data/Migrations/20230108083409_StockQuantity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WASMPointOfSale.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class StockQuantity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Stocks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Stocks");
+        }
+    }
+}
--- WASMPointOfSale/Server/Models/Stock.cs
+++ WASMPointOfSale/Server/Models/Stock.cs
@@ -6,6 +6,8 @@
 
         public int ProductId { get; set; }
 
+        public int Quantity { get; set; }
+
 
         public Product Product { get; set; }
     }
