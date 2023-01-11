6c428addf9fc0ddc95b99b122862443e75b7cd6b
Trystan Wilcock <trystanwilcock@hotmail.co.uk>
2023-01-11 06:01:02 +0000

    Added payment form.

---
+++ WASMPointOfSale/Shared/DTOs/AddPaymentDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WASMPointOfSale.Shared.DTOs
+{
+    public class AddPaymentDTO
+    {
+        public AddPaymentDTO(decimal amountDue)
+        {
+            AmountDue = amountDue;
+        }
+
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Amount cannot be below 0.")]
+        [Display(Name = "Payment Amount")]
+        public decimal PaymentAmount { get; set; }
+
+        public decimal AmountDue { get; set; }
+
+        public decimal LeftToPay
+        {
+            get
+            {
+                return AmountDue - PaymentAmount;
+            }
+        }
+    }
+}
